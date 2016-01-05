e2a49f2c0e3da84a188264337496f1d7ea5ee4af
Andrew Anissi <andrew@wingedearth.com>
2016-01-04 17:32:11 -0800

    add controller functions for new book page and create book action

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,4 +3,19 @@ class BooksController < ApplicationController
   def index
   end
 
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+  end
+
+  private
+
+    def book_params
+      params.require(:book).permit(:title, :description, :author,
+        :book_uri, :image_uri, :featured)
+    end
+
 end
